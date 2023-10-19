e455c713c39d5daad304133222d7a3e7504f5701
neojinxd <neojinxd@gmail.com>
2023-10-18 21:32:52 -0400

    project setup

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import SignIn from '@/components/signin'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignIn />
+    </main>
+  )
+}
