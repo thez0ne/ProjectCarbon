cf01f728816290597dcabff3db4202f46bf7c379
neojinxd <neojinxd@gmail.com>
2023-11-03 17:57:42 -0400

    adding logos and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,37 @@ import Navbar from '@/components/navbar';
 
 export const metadata: Metadata = {
   title: 'Project Carbon',
-  description: 'an alternative chat app',
+  description: 'Another Alternative Chat App',
+  icons: {
+    shortcut: './carbonlogowbkg.svg',
+  },
+  openGraph: {
+    title: 'Project Carbon',
+    description: 'Welcome to Carbon, Another Alternative Chat App',
+    url: 'https://carbon.thez0ne.xyz',
+    siteName: 'the_z0ne',
+    images: [
+      {
+        url: 'https://thez0ne.xyz/carbonlogowbkg.svg',
+        width: 100,
+        height: 100,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  generator: 'Next.js',
+  applicationName: 'carbon',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'thez0ne', 'z0ne', 'the', 'socketio', 'socket.io', 'socket', 'io', 'chat', 'carbon', 'realtime', 'real-time', 'real', 'time'],
+  colorScheme: 'dark',
+  creator: 'Anik Patel',
+  publisher: 'Anik Patel',
+  formatDetection: {
+    email: true,
+    address: false,
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
